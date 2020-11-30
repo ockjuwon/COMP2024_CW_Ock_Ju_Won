2ed7022c0f650d36f734d7ee6e1b2f541d7b0cb1
ock ju won <ockjuwon99@gmail.com>
2020-11-30 23:41:10 +0900

    Make ImageProvider public

--- src/main/java/frogger/data/ImageProvider.java
+++ src/main/java/frogger/data/ImageProvider.java
@@ -4,7 +4,7 @@ package frogger.data;
 import javafx.scene.image.Image;
 
 public abstract class ImageProvider {
-    static Image getImage(String url, int imageSize) {
+    public static Image get(String url, int imageSize) {
         return new Image(url, imageSize, imageSize, true, true);
     }
 }
