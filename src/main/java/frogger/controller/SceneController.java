9feb2097c4cee38126b46b036ed56b25858cbf52
ock ju won <ockjuwon99@gmail.com>
2020-12-02 22:19:16 +0900

    Make SceneController use cache && Stage won alert

--- src/main/java/frogger/controller/SceneController.java
+++ src/main/java/frogger/controller/SceneController.java
@@ -5,8 +5,11 @@ import frogger.model.MusicPlayer;
 import frogger.model.ScoreBoard;
 import frogger.model.World;
 import frogger.view.*;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -16,13 +19,15 @@ import java.util.HashMap;
 
 public class SceneController extends World {
     private static SceneController instance;
-    private final HashMap<String, Pane> sceneHashMap = new HashMap<>();
+    private final HashMap<String, Pane> paneHashMap = new HashMap<>();
+    private final HashMap<String, Scene> sceneHashMap = new HashMap<>();
     private String sceneName = null;
     private Pane currentPane = null;
     private final Stage primaryStage;
     static final int WORLD_WIDTH = 600;
     static final int WORLD_HEIGHT = 800;
     private MusicPlayer musicPlayer;
+    private int endCount = 0;
 
 
     public SceneController(Stage primaryStage) {
@@ -45,19 +50,46 @@ public class SceneController extends World {
     }
 
     private void addScreen() {
-        sceneHashMap.put("start", new Start());
-        sceneHashMap.put("info", new Info());
-        sceneHashMap.put("win", new Win());
-        sceneHashMap.put("level1", new Level1());
-        sceneHashMap.put("level2", new Level2());
-        sceneHashMap.put("level3", new Level3());
-        sceneHashMap.put("level4", new Level4());
-        sceneHashMap.put("level5", new Level5());
-        sceneHashMap.put("level6", new Level6());
-        sceneHashMap.put("level7", new Level7());
-        sceneHashMap.put("level8", new Level8());
-        sceneHashMap.put("level9", new Level9());
-        sceneHashMap.put("level10", new Level10());
+        paneHashMap.put("start", new Start());
+        paneHashMap.put("info", new Info());
+        paneHashMap.put("win", new Win());
+        paneHashMap.put("level1", new Level1());
+        paneHashMap.put("level2", new Level2());
+        paneHashMap.put("level3", new Level3());
+        paneHashMap.put("level4", new Level4());
+        paneHashMap.put("level5", new Level5());
+        paneHashMap.put("level6", new Level6());
+        paneHashMap.put("level7", new Level7());
+        paneHashMap.put("level8", new Level8());
+        paneHashMap.put("level9", new Level9());
+        paneHashMap.put("level10", new Level10());
+
+        for(String key : paneHashMap.keySet()) {
+            Pane pane = paneHashMap.get(key);
+            Scene scene = new Scene(pane, WORLD_WIDTH, WORLD_HEIGHT);
+            scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+                final String storedKey = key;
+                @Override
+                public void handle(KeyEvent event) {
+                    if(event.getCode() == KeyCode.ESCAPE) {
+                        if(storedKey.equals("start")) {
+                            return;
+                        }
+                        changeScene("start");
+                    }
+                }
+            });
+            sceneHashMap.put(key, scene);
+        }
+    }
+
+    public void touchEnd(Animal animal) {
+        endCount++;
+        if(endCount < 5) {
+            return;
+        }
+        endCount = 0;
+        goNextLevel(animal);
     }
 
     public void goNextLevel(Animal animal) {
@@ -72,6 +104,14 @@ public class SceneController extends World {
         }
         idx++;
         String nextScene = String.format("level%d", idx);
+        musicPlayer.stopMusic();
+        if(sceneName.startsWith("level")) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("You Have Won The Stage!");
+            alert.setHeaderText("Your Current Score: "+animal.getPoint()+"!");
+            alert.setContentText("Highest Possible Score: " + idx * 100);
+            alert.show();
+        }
         changeScene(nextScene);
     }
 
@@ -80,7 +120,7 @@ public class SceneController extends World {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("You Have Won The Game!");
         alert.setHeaderText("Your High Score: "+animal.getPoint()+"!");
-        alert.setContentText("Highest Possible Score: 800");
+        alert.setContentText("Highest Possible Score: 1000");
         alert.show();
     }
 
@@ -90,9 +130,10 @@ public class SceneController extends World {
             ((World) currentPane).stop();
         }
         musicPlayer.stopMusic();
-        currentPane = sceneHashMap.get(sceneName);
+        currentPane = paneHashMap.get(sceneName);
         primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(currentPane, WORLD_WIDTH, WORLD_HEIGHT));
+        Scene scene = sceneHashMap.get(sceneName);
+        primaryStage.setScene(scene);
         primaryStage.show();
         if(sceneName.startsWith("level")) {
             musicPlayer.playMusic();
