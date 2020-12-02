2408e79d5ea797555c16b7d615793a880fd4ec26
ock ju won <ockjuwon99@gmail.com>
2020-12-02 19:04:14 +0900

    Refactor End

--- src/main/java/frogger/model/End.java
+++ src/main/java/frogger/model/End.java
@@ -4,10 +4,9 @@ import javafx.scene.image.Image;
 
 public class End extends Actor{
 	boolean activated = false;
+
 	@Override
-	public void act(long now) {
-		// TODO Auto-generated method st
-	}
+	public void act(long now) { }
 	
 	public End(int x, int y) {
 		setX(x);
