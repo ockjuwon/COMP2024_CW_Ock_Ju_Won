dae7589a6908b565de7c174229e532f24b42d448
ock ju won <ockjuwon99@gmail.com>
2020-12-04 15:04:06 +0900

    make use backbutton

--- src/main/java/frogger/view/Info.java
+++ src/main/java/frogger/view/Info.java
@@ -2,8 +2,10 @@ package frogger.view;
 
 import frogger.controller.SceneController;
 import frogger.model.Animal;
+import frogger.model.StartButton;
 import frogger.model.World;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -15,6 +17,7 @@ public class Info extends Pane {
     private static final int infoWidth = 600;
     private static final int infoHeight = 800;
     private static final String IMAGE = "file:src/main/resources/0.png";
+    private StartButton backButton;
 
     BackgroundImage infoImage = new BackgroundImage(new Image(IMAGE, infoWidth, infoHeight, false, true),
             BackgroundRepeat.NO_REPEAT, null, BackgroundPosition.DEFAULT, null);
@@ -22,6 +25,11 @@ public class Info extends Pane {
     public Info() {
         setPrefSize(infoWidth, infoHeight);
         setBackground(new Background(infoImage));
+        backButton = new StartButton("BACK");
+        backButton.setOnAction((event) -> {
+            SceneController.getInstance().changeScene("start");
+        });
+        getChildren().add(backButton);
     }
 
 }
