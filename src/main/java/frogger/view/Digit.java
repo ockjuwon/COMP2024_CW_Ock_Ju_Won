e75a4a1231b6d537dc78af013125a4c11b08cfe9
ock ju won <ockjuwon99@gmail.com>
2020-12-05 20:55:54 +0900

    Add digit javadoc

--- src/main/java/frogger/view/Digit.java
+++ src/main/java/frogger/view/Digit.java
@@ -4,12 +4,22 @@ import frogger.controller.ImageProvider;
 import frogger.model.Actor;
 import javafx.scene.image.Image;
 
+/**
+ * A class represents digit.
+ */
 public class Digit extends Actor {
 	@Override
 	public void act(long now) {
 
 	}
-	
+
+	/**
+	 * Constructs digit.
+	 * @param n digit
+	 * @param dim size
+	 * @param x x position
+	 * @param y y position
+	 */
 	public Digit(int n, int dim, int x, int y) {
 		String format = "file:src/main/resources/" + n + ".png";
 		Image im1 = ImageProvider.get(format, dim);
