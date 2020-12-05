5706fd03d1ed5086300c6341a9306825df9f2310
ock ju won <ockjuwon99@gmail.com>
2020-12-05 20:17:19 +0900

    Add javadoc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,21 @@ public class Main extends Application {
 	private SceneController sceneController;
 
 
+	/**
+	 * starts SceneController.
+	 * @param primaryStage given stage.
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		sceneController = new SceneController(primaryStage);
 		sceneController.start();
 	}
 
+	/**
+	 * Entrypoint of program.
+	 * launches application.
+	 * @param args program's arguments.
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
