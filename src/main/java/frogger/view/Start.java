cad76f7c0c092177f9415402a532c906ac7a1b31
ockjuwon <61516629+ockjuwon@users.noreply.github.com>
2020-12-06 13:48:32 +0800

    Update Start.java

--- src/main/java/frogger/view/Start.java
+++ src/main/java/frogger/view/Start.java
@@ -2,6 +2,12 @@ package frogger.view;
 
 import frogger.controller.SceneController;
 import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
@@ -9,8 +15,15 @@ import java.util.List;
 
 public class Start extends Pane {
 
-    private static final int MENU_BUTTONS_START_X = 100;
-    private static final int MENU_BUTTONS_START_Y = 150;
+    private static final int sceneWidth = 600;
+    private static final int sceneHeight = 800;
+    private static final String IMAGE = "file:src/main/resources/startBackground.png";
+    private static final int MENU_BUTTONS_START_X = 205;
+    private static final int MENU_BUTTONS_START_Y = 250;
+    
+    BackgroundImage startImage = new BackgroundImage(new Image(IMAGE, sceneWidth, sceneHeight, false, true),
+            BackgroundRepeat.NO_REPEAT, null, BackgroundPosition.DEFAULT, null);
+
     private SceneController sceneController;
 
     // define list to store these buttons
@@ -18,13 +31,32 @@ public class Start extends Pane {
 
     public Start() {
         sceneController = SceneController.getInstance();
-
         menuButtons = new ArrayList<>();
-
+        setBackground(new Background(startImage));
+        createLogo();
+        createFrogImage();
         createStartButton();
         createInfoButton();
         createExitButton();
 
+    }
+    
+        private void createLogo() {
+        ImageView logo = new ImageView("file:src/main/resources/froggerLogo.png");
+        logo.setFitHeight(200);
+        logo.setFitWidth(500);
+        logo.setLayoutX(50);
+        logo.setLayoutY(30);
+        getChildren().add(logo);
+    }
+
+    private void createFrogImage() {
+        ImageView frogImage = new ImageView("file:src/main/resources/froggerImage.png");
+        frogImage.setFitHeight(200);
+        frogImage.setFitWidth(500);
+        frogImage.setLayoutX(50);
+        frogImage.setLayoutY(550);
+        getChildren().add(frogImage);
     }
 
     private void addMenuButton(StartButton button) {
